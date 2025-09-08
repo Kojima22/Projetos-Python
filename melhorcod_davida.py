d14d5bfa66c4bb7cc3ac1f9e976286ba11131134
Kojima22 <zinbg494@gmail.com>
2025-09-08 16:58:20 -0300

    Create melhorcod_davida.py
    best cod of the word!

---
+++ melhorcod_davida.py
@@ -0,0 +1,34 @@
+#cod base
+print('Seja bem vindo ao codigo tronxo')
+
+confirm = input("Esta afim de aprender algo: ")
+if confirm == 'sim':
+    print('Entao vamboras')
+elif confirm == 'não':
+  print('Já sabe, se lasca')
+
+#vatifull
+print('Oque você gostaria de aprender?')
+confirm = input('Calculos, codigos ou ver uma imagem secreta? ')
+
+if confirm == 'codigos'  :
+    print('então acesse esse arquivo Calculadora.py')
+
+elif confirm == 'calculos' : 
+  print('então toma essa merda ai 1+1=2')
+
+elif confirm == 'imagem secreta' : 
+  print("""Então olha isso aqui:
+⠀⠀⠀⠀⠀⠀⠀⣠⣶⣶⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣴⣶⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⠀⠀ VOCÊ
+⠀⠀⠀⠀⣼⣿⣿⣿⡿⣿⣿⣆⠀⠀⠀⠀⠀⠀⣠⣴⣶⣤⡀⠀
+⠀⠀⠀⢰⣿⣿⣿⣿⠃⠈⢻⣿⣦⠀⠀⠀⠀⣸⣿⣿⣿⣿⣷⠀
+⠀⠀⠀⠘⣿⣿⣿⡏⣴⣿⣷⣝⢿⣷⢀⠀⢀⣿⣿⣿⣿⡿⠋⠀
+⠀⠀⠀⠀⢿⣿⣿⡇⢻⣿⣿⣿⣷⣶⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⣿⣿⣇⢸⣿⣿⡟⠙⠛⠻⣿⣿⣿⣿⡇⠀⠀⠀⠀
+⣴⣿⣿⣿⣿⣿⣿⣿⣠⣿⣿⡇⠀⠀⠀⠉⠛⣽⣿⣇⣀⣀⣀⠀
+⠙⠻⠿⠿⠿⠿⠿⠟⠿⠿⠿⠇⠀⠀⠀⠀⠀⠻⠿⠿⠛⠛⠛⠃""")
